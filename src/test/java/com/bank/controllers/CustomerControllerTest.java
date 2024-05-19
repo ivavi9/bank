f16048608aa70c8a0e150bdcfdebea18cd4b124e
ivavi9 <25426909+ivavi9@users.noreply.github.com>
2024-05-19 14:41:56 +0530

    added test to check CustomerController CreateCustomer functionality

---
+++ src/test/java/com/bank/controllers/CustomerControllerTest.java
@@ -0,0 +1,60 @@
+package com.bank.controllers;
+
+
+import com.bank.dtos.CustomerDTO;
+import com.bank.models.Customer;
+import com.bank.services.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.http.MediaType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerControllerTest {
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    MockMvc mockMvc;
+
+    @Test
+    void testCreateCustomer() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        CustomerDTO customer = new CustomerDTO();
+        customer.setFirstName("John");
+        customer.setLastName("Doe");
+        customer.setEmail("somethingemail@gmail.com");
+        customer.setPhone("123456789");
+        customer.setAddress("1234, Some Street, Some City, Some Country");
+
+
+        when(customerService.addNewCustomer(any(CustomerDTO.class))).thenReturn(customer);
+        mockMvc.perform(post("/api/v1/customers/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"John\",\n" +
+                        "    \"lastName\": \"Doe\",\n" +
+                        "    \"email\": \"somethingemail@gmail.com\",\n" +
+                        "    \"phone\": \"123456789\",\n" +
+                        "    \"address\": \"1234, Some Street, Some City, Some Country\"\n }"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.email").value("somethingemail@gmail.com"))
+                .andExpect(jsonPath("$.phone").value("123456789"))
+                .andExpect(jsonPath("$.address").value("1234, Some Street, Some City, Some Country"));
+
+    }
+}
